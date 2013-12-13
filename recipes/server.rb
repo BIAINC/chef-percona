9ed0264e5770e397dac4b516d53a907b44bcdeed
paustin01 <paustin@biaprotect.com>
2013-12-12 16:48:13 -0800

    Hacked version locking it to percona 5.6.  Final solve is to get encrypted data bags going.

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,7 @@
 include_recipe "percona::package_repo"
 
 # install packages
-package "percona-server-server" do
+package "percona-server-server-5.6" do
   action :install
   options "--force-yes"
 end
